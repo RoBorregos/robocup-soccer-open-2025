2339cf146e6d57caa53b03f30a865a53171c1cbd
emilwinkp <181249743+emilwinkp@users.noreply.github.com>
2025-03-24 20:16:00 -0600

    Updated logic for goalkeeper

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,4 @@
-#include <PWMServo.h>  
+#include <Servo.h>  
 #include "motores.h"
 #include "constantes.h"
 #include "Bno.h"
@@ -40,7 +40,10 @@ const int servo_min = 1000;
 const int servo_mid = 1500;
 const int servo_max = 2000;
 float time_shoot = 2000;
-
+float defense_limit = 0;
+const int distance_limit_min = 30;
+const int distance_limit_max = 40;
+bool defense_zone = false;
 
 BNO055 bno;
 PWMServo dribbler;
@@ -92,6 +95,12 @@ void loop() {
     open_ball_seen = (ball_distance != 0 || ball_angle != 0);
   }
 
+  if (goal_distance > distance_limit_min && goal_distance < distance_limit_max){
+    defense_zone = true;
+  } else {
+    defense_zone = false;
+  }
+
   photo.ReadPhotoBack
   photo.ReadPhotoFront
   photo.ReadPhotoLeft
@@ -102,29 +111,34 @@ void loop() {
   double speed_goal = 155;
   double speed_ball = 200;
 
-  goal_angle_180 = goal_angle + 180;
-  
-  if (open_ball_seen){
+  defense_limit = ball_distance + goal_distance;
+
+
+  if (open_ball_seen && defense_limit < 70 && defense_zone){ //Checar valores de distancia
     if (ball_angle != 0){
-      double error_ball = bno.analize_error(ball_angle, current_yaw);
+      double error_ball = ball_angle + current_yaw;
       double differential_ball = error_ball * 0.001; //Calcular el error diferecial
       ponderated_ball = ball_angle + differential_ball;
       motors.MoveMotorsImu(ponderated_ball, abs(speed_ball), speed_w);
     } else {
-      motors.StopMotors();
+      double error_goal = goal_angle + current_yaw;
+      double differential_goal = error_goal * 0.001; //Calcular el error diferencial por medio de prueba y error
+      ponderated_goal = goal_angle + differential_goal;
+      motors.MoveMotorsImu(ponderated_goal, abs(speed_goal), speed_w);
     }
   } else {
-    if (goal_angle != 180 && goal_distance > 30){//Distance to the penalti area
-      double error_goal = bno.analize_error(goal_angle_180, current_yaw);
-      double differential_goal = error_goal * 0.001; //Calcular el error diferencial por medio de prueba y error
-      ponderated_goal = goal_angle_180 + differential_goal;
-      motors.MoveMotorsImu(ponderated_goal, abs(speed_goal), speed_w);
-      if (photo.ReadPhotoLeft && photo.ReadPhotoRight){
-        motors.StopMotors();
-      }
+    motors.MoveMotorsImu(0, 0, speed_w);
+    if (goal_angle != 180 && goal_angle >  0) {
+      motors.MoveRight();
+    } else if (goal_angle != 180 && goal_angle < 0) {
+      motors.MoveLeft();
+    } else if (goal_angle == 180) {
+      motors.StopMotors();
     }
   }
+
 }
+
 
 
 
