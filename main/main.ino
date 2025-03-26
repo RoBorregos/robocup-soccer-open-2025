0c65367f5b196ecdd0015be90747c196e028363e
emilwinkp <181249743+emilwinkp@users.noreply.github.com>
2025-03-25 23:07:37 -0600

    Libreria correcta de Servo

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,10 @@
-#include <PWMServo.h>  
+#include <Servo.h>  
 #include "motores.h"
 #include "constantes.h"
 #include "Bno.h"
 #include "PID.h"
 #include <cmath>
 
-//Incluir la libreria del servo para el Kicker
-//#include "Photo.h"
 
 float bno_angle = 0;
 unsigned long start_millis;
@@ -36,99 +34,27 @@ const int servo_min = 1000;
 const int servo_mid = 1500;
 const int servo_max = 2000;
 
-BNO055 bno;
-PWMServo servo;
-//Photo photo;
-
-PID pid(0.65, 0.01 , 0.6, 200);
-
-Motors motors(
-    MOTOR1_PWM, MOTOR1_IN1, MOTOR1_IN2,
-    MOTOR2_PWM, MOTOR2_IN1, MOTOR2_IN2,
-    MOTOR3_PWM, MOTOR3_IN1, MOTOR3_IN2,
-    MOTOR4_PWM, MOTOR4_IN1, MOTOR4_IN2);
+Servo dribbler;
 
 void setup() {
-  Serial.begin(115200);
-  motors.InitializeMotors();
-  bno.InitializeBNO();
-  servo.attach(0); //Pin de control de señal 
-  //photo.InitializeADS();
+    Serial1.begin(115200);
+    Serial.begin(115200);
+    dribbler.attach(6);
+    Serial1.begin(115200);
 }
 
-double radians_degrees(double radians) {
-  return radians * (180.0 / M_PI);
+int microsecondsToDegrees(int microseconds) {
+    return map(microseconds, 1000, 2000, 0, 180);
 }
 
 void loop() {
-  bno.GetBNOData();
-  double current_yaw = bno.GetYaw();
-
-  if (Serial1.available()){
-    int bytesRead = Serial1.readBytesUntil('\n', buffer, sizeof(buffer));
-    buffer[bytesRead] = '\0';
-    char* token = strtok(buffer, " ");
-    ball_distance = atof(token);
-
-    token = strtok(NULL, " ");
-    ball_angle = atof(token);
-
-    token = strtok(NULL, " ");
-    goal_angle = atof(token);
-
-    token = strtok(NULL, " ");
-    goal_distance = atof(token);
-
-    open_ball_seen = (ball_distance != 0 || ball_angle != 0);
-  }
-
-  double error = bno.analize_error(setpoint,current_yaw);
-  double speed_w = pid.Calculate(setpoint, error); //Checar si esta bien asi o hay que invertir los valores y aplicar la logica para los diversos casos
-  double speed_goal = 155;
-  double speed_ball = 200;
-  
-  if (speed_w != 0) { 
-    if (open_ball_seen){
-      double error_ball = bno.analize_error(ball_angle, current_yaw);
-      double differential_ball = error_ball * 0.001; //Calcular el error diferecial 
-      ponderated_ball = ball_angle + differential_ball;
-      motors.MoveMotorsImu(ponderated_ball, abs(speed_ball), speed_w);
-      //Acivar Dribbler
-      if (goal_angle != 0) {
-      double error_goal = bno.analize_error(goal_angle, current_yaw);
-      double differential_goal = error_goal * 0.001; //Calcular el error diferencial por medio de prueba y error
-      ponderated_goal = goal_angle + differential_goal;
-      motors.MoveMotorsImu(ponderated_goal, abs(speed_goal), speed_w);
-      }
-    }
-    if (open_ball_seen){
-      double error_ball =  bno.analize_error(ball_angle, current_yaw);
-      double differential_ball = error_ball * 0.001; //Calcular el error diferecial por medio de experimentacion 
-      ponderated_ball = ball_angle + differential_ball;
-      motors.MoveMotorsImu(ponderated_ball, abs(speed_ball), speed_w);
-    }
-
-    if (!open_ball_seen) {
-      if (goal_angle != 0 ) {
-        double error_goal = bno.analize_error(goal_angle, current_yaw);
-        double differential_goal = error_goal * 0.001; //Calcular el error diferencial por medio de prueba y error
-        ponderated_goal = goal_angle + differential_goal;
-        if (goal_distance > 20){ //Checar si se puede medir la distancia con la OpenMV
-          motors.StopMotors();
-          //Disparar pelota con Kicker
-        } 
-      }
-    }
-//Case of nothing detected
-    if (!open_ball_seen) {
-      motors.StopMotors();
-    }
-}
+    dribbler.writeMicroseconds(servo_mid);
+    delay(1000);
+    dribbler.writeMicroseconds(servo_min);
+    delay(1000);
+    dribbler.writeMicroseconds(servo_max);
+    delay(1000);
+    dribbler.writeMicroseconds(0);
+    delay(5000);
 }
 
-/*
-Distancias de la Pixy. 318 x 208
-*/
-  
-  
-  
