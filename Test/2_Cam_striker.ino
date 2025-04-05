e6fed631e6d683dffaf5b1940cea3687526fa16a
emilwinkp <181249743+emilwinkp@users.noreply.github.com>
2025-04-05 14:10:40 -0600

    Changes for 2 cam logic

--- Test/2_Cam_striker.ino
+++ Test/2_Cam_striker.ino
@@ -7,6 +7,7 @@
 #include <cmath>
 //Incluir la libreria del servo para el Kicker
 
+
 float bno_angle = 0;
 unsigned long start_millis;
 unsigned long current_millis;
@@ -17,6 +18,7 @@ float adjust_angle = 0;
 float angle_degrees = 0;
 float ponderated_ball = 0;
 float ponderated_goal = 0;
+float ponderated_dribbler = 0;
 float ball_angle = 0;
 float dribbler_distance = 0;
 float dribbler_angle = 0;
@@ -49,6 +51,7 @@ Servo dribbler;
 
 
 PID pid(4, 0.01 , 0.6, 500); //0.6, 0.01, 0.6, 200
+PID pid2();
 
 
 Motors motors(
@@ -57,20 +60,23 @@ Motors motors(
     MOTOR3_PWM, MOTOR3_IN1, MOTOR3_IN2,
     MOTOR4_PWM, MOTOR4_IN1, MOTOR4_IN2);
     
-PhotoSensors sensors(front, left, right, back);
+//PhotoSensors sensors(front, left, right, back);
 
 
 void setup() {
   Serial.begin(115200);
+  Serial2.begin(115200);
   Serial1.begin(115200);
   dribbler.attach(6);
   dribbler.writeMicroseconds(servo_min);
   motors.InitializeMotors();
-  bno.InitializeBNO();  
+  bno.InitializeBNO(); 
+/* 
   sensors.setThreshold(FRONT, 600);
   sensors.setThreshold(LEFT,  580);
   sensors.setThreshold(RIGHT, 590);
   sensors.setThreshold(BACK,  610);
+*/
   delay(1000);
 
 }
@@ -79,48 +85,47 @@ void setup() {
 void loop() { 
   bno.GetBNOData();
   double current_yaw = bno.GetYaw();
+  if (!dribbler_ball_seen) {
+    if (Serial2.available()){
+      Serial.println("Reading data from OpenMV 1...");
+      int bytesRead = Serial2.readBytesUntil('\n', buffer2, sizeof(buffer2));
+      buffer2[bytesRead] = '\0';
 
+      Serial.print("Raw Data 1: ");
+      Serial.println(buffer2);
+
+      char* token = strtok(buffer2, " ");
+      ball_distance = atof(token);
+
+
+
+      token = strtok(NULL, " ");
+      ball_angle = atof(token);
+
+
+      token = strtok(NULL, " ");
+      goal_angle = atof(token);
+
+
+      token = strtok(NULL, " ");
+      goal_distance = atof(token);
+
+
+      open_ball_seen = (ball_distance != 0 && ball_angle != 0);
+      goal_seen = (goal_angle != 0 && goal_distance != 0);
+      delay(50);
+    }
+  }
 
   if (Serial1.available()){
-    Serial.println("Reading data from OpenMV...");
+    Serial.println("Reading data from OpenMV 2...");
     int bytesRead = Serial1.readBytesUntil('\n', buffer1, sizeof(buffer1));
-    buffer[bytesRead] = '\0';
+    buffer1[bytesRead] = '\0';
 
-    Serial.print("Raw Data: ");
+    Serial.print("Raw Data 2: ");
     Serial.println(buffer1);
 
     char* token = strtok(buffer1, " ");
-    ball_distance = atof(token);
-
-
-
-    token = strtok(NULL, " ");
-    ball_angle = atof(token);
-
-
-    token = strtok(NULL, " ");
-    goal_angle = atof(token);
-
-
-    token = strtok(NULL, " ");
-    goal_distance = atof(token);
-
-
-    open_ball_seen = (ball_distance != 0 && ball_angle != 0);
-    goal_seen = (goal_angle != 0 && goal_distance != 0);
-    delay(50);
-
-  }
-
-  if (Serial2.available()){
-    Serial.println("Reading data from OpenMV...");
-    int bytesRead = Serial2.readBytesUntil('\n', buffer2, sizeof(buffer2));
-    buffer2[bytesRead] = '\0';
-
-    Serial.print("Raw Data: ");
-    Serial.println(buffer2);
-
-    char* token = strtok(buffer2, " ");
     dribbler_distance = atof(token);
 
 
@@ -128,7 +133,6 @@ void loop() {
     token = strtok(NULL, " ");
     dribbler_angle = atof(token);
 
-
     token = strtok(NULL, " ");
     goal_angle = atof(token);
 
@@ -136,55 +140,64 @@ void loop() {
     token = strtok(NULL, " ");
     goal_distance = atof(token);
 
-
-    dribbler_ball_seen = (dribbler_distance != 0 && dribbler_angle != 0);
-    ball_captured = (dribbler_distance <= 40 && dribbler_angle == 0);
+    dribbler_ball_seen = (dribbler_distance != 0 || dribbler_angle != 0);
+    if (dribbler_distance == 0 && dribbler_angle == 0) {
+      dribbler_ball_seen = false;
+    }
+    
+    ball_captured = (dribbler_distance <= 20  && dribbler_angle == 0);
+    if (dribbler_angle != 0) {
+      ball_captured = false;
+    }
     delay(50);
   }
+
 
   double error = bno.analize_error(setpoint,current_yaw);
   double speed_w = pid.Calculate(setpoint, error); //Checar si esta bien asi o hay que invertir los valores y aplicar la logica para los diversos casos
   double speed_goal = 155;
   double speed_ball = 100;
  
-  if (sensors.isLineDetected(FRONT)) {
-    Serial.println("Línea al frente. Retrocede.");
-    motors.MoveMotorsImu(180, 200, speed_w);
-    delay(300);
-  } else if (sensors.isLineDetected(LEFT)) {
-    Serial.println("Línea a la izquierda. Gira a la derecha.");
-    motors.MoveMotorsImu(90, 200, speed_w);
-    delay(300);
-  } else if (sensors.isLineDetected(RIGHT)) {
-    Serial.println("Línea a la derecha. Gira a la izquierda.");
-    motors.MoveMotorsImu(270, 200, speed_w);
-    delay(300);
-  } else if (sensors.isLineDetected(BACK)) {
-    Serial.println("Línea atrás. Avanza.");
-    motors.MoveMotorsImu(0, 200, speed_w);
-    delay(300);
-  }
-  if (dribbler_ball_seen){
-      double error_dribbler = dribbler_angle + current_yaw;
-      double differential_dribbler = error_dribbler * 0.001; //Calcular el error diferecial
-      ponderated_dribbler = -(dribbler + differential_ball);
-      motors.MoveMotorsImu(ponderated_dribbler, abs(speed_ball), speed_w);
-      dribbler.writeMicroseconds(servo_mid);
-  }
-  if (open_ball_seen){
+
+  if (open_ball_seen && !dribbler_ball_seen){
       double error_ball = ball_angle + current_yaw;
       double differential_ball = error_ball * 0.001; //Calcular el error diferecial
-      ponderated_ball = -(ball_angle + differential_ball);
+      ponderated_ball = (ball_angle + differential_ball);
       motors.MoveMotorsImu(ponderated_ball, abs(speed_ball), speed_w);
-      dribbler.writeMicroseconds(servo_mid);
+  } 
+  else if (dribbler_ball_seen){
+    double error_dribbler = dribbler_angle + current_yaw;
+    double differential_dribbler = error_dribbler * 0.001; //Calcular el error diferecial
+    ponderated_dribbler = -(dribbler_angle + differential_dribbler);
+    motors.MoveMotorsImu(ponderated_dribbler, abs(speed_ball), speed_w);
+    dribbler.writeMicroseconds(servo_mid);
   } else {
       motors.MoveMotorsImu(0,0,speed_w);
       if (speed_w == 0) {
-        motors.SetAllSpeeds(80);
-        motors.MoveBackward();
+        //motors.SetAllSpeeds(80);
+        //motors.MoveBackward();
+        motors.SetAllSpeeds(0);
+        motors.StopMotors();
       }
     }
-
-    
 }
+/*
+    if (sensors.isLineDetected(FRONT)) {
+      Serial.println("Línea al frente. Retrocede.");
+      motors.MoveMotorsImu(180, 200, speed_w);
+      delay(300);
+    } else if (sensors.isLineDetected(LEFT)) {
+      Serial.println("Línea a la izquierda. Gira a la derecha.");
+      motors.MoveMotorsImu(90, 200, speed_w);
+      delay(300);
+    } else if (sensors.isLineDetected(RIGHT)) {
+      Serial.println("Línea a la derecha. Gira a la izquierda.");
+      motors.MoveMotorsImu(270, 200, speed_w);
+      delay(300);
+    } else if (sensors.isLineDetected(BACK)) {
+      Serial.println("Línea atrás. Avanza.");
+      motors.MoveMotorsImu(0, 200, speed_w);
+      delay(300);
+    }
+       */
 
---
+++ Vision/Dribbler_Cam_2_control.py
@@ -0,0 +1,178 @@
+#Untitled - By: Emil - Wed Mar 19 2025
+
+import sensor
+import time
+import utime
+import pyb
+import math
+import image
+from pyb import UART
+
+# Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
+
+uart = UART(3, 115200, timeout_char=0)
+uart.init(115200, bits=8, parity=None, stop=1)
+threshold = (65, 37, 15, 45, 8, 28) #(49, 84, 16, 39, 6, 100)#9, 93, 26, 92, -34, 78)(43, 100, 14, 39, -53, 76) (16, 100, 12, 127, -11, 127) (0, 100, -128, 127, -128, 127)
+threshold_2 = (0, 35, 33, -10, -122, -14) #Azul
+threshold_1 = (50, 73, -55, 48, 8, 28) #(4, 94, 29, 46, 4, 45) (99, 25, 20, -83, 127, 40) Amarillo esta bien para 0,0,0
+
+# The reference point for angle and distance are set in the folloxing coordinates
+
+# The resolution of the cam is 320x240
+
+# X and Y coordinates for a 50° of Dribbler cam
+
+X_CENTER = 157 #182
+Y_CENTER = 180 #175 #166
+
+# X and Y coordinates for a 60° of Dribbler cam
+
+#X_CENTER = 195 #182
+#Y_CENTER = 169 #175 #166
+
+def initialize_open():
+    led = pyb.LED(1)  # 1 = Rojo, 2 = Verde, 3 = Azul
+    led.on()
+    time.sleep(1)  # Mantener el LED encendido por 1 segundo
+    led.off()
+    sensor.reset()
+    sensor.set_pixformat(sensor.RGB565)
+    sensor.set_framesize(sensor.QVGA)
+    print("Final Gain Ceiling:", sensor.get_gain_db())#Checar el valor de gain para H7
+    print("Final Exposure:", sensor.get_exposure_us()) #Checar el valor de exposicion H7
+    sensor.skip_frames(time=4000)
+    sensor.set_auto_gain(False)
+    sensor.set_gainceiling(16)
+    sensor.set_auto_whitebal(False)  # must be turned off for color tracking
+    sensor.set_auto_exposure(False, exposure_us=5000) #100
+    sensor.set_brightness(-5)
+    sensor.set_hmirror(True)
+    sensor.set_vflip(False)
+    sensor.set_transpose(True)
+    sensor.set_contrast(-3)
+    sensor.set_saturation(-3)
+
+
+def find_ball(img):
+    blob_ball = img.find_blobs([threshold], area_threshold=30, merge=True) #Checar los valores de area y pixeles
+    for blob in blob_ball:
+        img.draw_rectangle(blob.rect(), color = (255, 0, 0))
+        img.draw_cross(blob.cx(), blob.cy(), color = (255, 0, 0))
+
+    return blob_ball
+
+def find_goal(img):
+    blob_goal = img.find_blobs([threshold_1], pixels_threshold=200, area_threshold=800, merge=True) #Checar los valores de area y pixeles
+    for blob in blob_goal:
+        img.draw_rectangle(blob.rect(), color = (0, 255, 0))
+        img.draw_cross(blob.cx(), blob.cy(), color = (0, 255, 0))
+
+    return blob_goal
+
+def find_goal_opp(img):
+    blob_goal_opp = img.find_blobs([threshold_2], pixels_threshold=200, area_threshold=800, merge=True) #Checar los valores de area y pixeles
+    for blob in blob_goal_opp:
+        img.draw_rectangle(blob.rect(), color = (0, 0, 255))
+        img.draw_cross(blob.cx(), blob.cy(), color = (0, 0, 255))
+
+    return blob_goal_opp
+
+def distance_ball(blob):
+    relative_distx = blob.cx() - X_CENTER
+    relative_disty = blob.cy() - Y_CENTER
+
+    vector_dist = math.sqrt(relative_distx**2 + relative_disty**2)
+    distance =vector_dist
+    #print("Distance: %d" % distance)
+
+    return distance
+
+def distance_goal(blob):
+    relative_distx = blob.cx() - X_CENTER
+    relative_disty = blob.cy() - Y_CENTER
+    #print("Relative Distx: %d" % relative_distx)
+    #print("Relative Disty: %d" % relative_disty)
+
+    vector_dist = math.sqrt(relative_distx**2 + relative_disty**2)
+    distance = vector_dist
+    #print("Distance: %d" % distance)
+
+    return distance
+
+def angle(blob):
+    relative_distx = blob.cx() - X_CENTER
+    relative_disty = blob.cy() - Y_CENTER
+    print("Relative Distx: %d" % relative_distx)
+    print("Relative Disty: %d" % relative_disty)
+
+    angle = math.atan2(relative_disty, relative_distx)
+    angle_degree = math.degrees(angle)
+    if angle_degree < 0:
+        angle_degree = 360 + angle_degree
+    return angle_degree
+
+
+def main():
+    initialize_open()
+    global distance_b, distance_g, distance_gop, angle_ball, angle_goal, angle_gop
+    clock = time.clock()
+    distance_b = 0
+    distance_g = 0
+    angle_ball = 0
+    angle_goal = 0
+    distance_gop = 0
+    angle_gop = 0
+
+    while True:
+        clock.tick()
+        img = sensor.snapshot()
+        blob_ball = find_ball(img)
+        blob_goal = find_goal(img)
+        blob_goal_opp = find_goal_opp(img)
+
+        if blob_ball:
+            for blob in blob_ball:
+                distance_b = distance_ball(blob)
+                angle_ball = -(angle(blob) - 180)
+                if angle_ball < 5 and angle_ball > -7:
+                    angle_ball = 0
+                elif distance_b < 30 and distance_b > -30:
+                    angle_ball = 0
+                print("Distance Ball: %d" % distance_b)
+                print("Angle Ball: %d" % angle_ball)
+
+        elif not blob_ball:
+            distance_b = 0
+            angle_ball = 0
+
+        if blob_goal:
+            for blob in blob_goal:
+                distance_g = distance_goal(blob)
+                angle_goal = -(angle(blob) - 180)
+                #print("Distance Goal: %d" % distance_g)
+                #print("Angle Goal: %d" % angle_goal)
+
+
+        elif not blob_goal:
+            distance_g = 0
+            angle_goal = 0
+
+        if blob_goal_opp:
+            for blob in blob_goal_opp:
+                distance_gop = distance_goal(blob)
+                angle_gop = -(angle(blob) - 180)
+                #print("Distance Opposite Goal: %d" % distance_gop)
+                #print("Angle Opposite Goal: %d" % angle_goal)
+
+        elif not blob_goal_opp:
+            distance_g = 0
+            angle_goal = 0
+
+    if distance_b != 0 and angle_ball != 0:
+        data = "{} {} {} {} {} {}\n".format(distance_b, angle_ball, distance_g, angle_goal, distance_gop, angle_gop)
+        print("Sending: ", data)
+        uart.write(data)
+        pyb.delay(50)
+
+if __name__ == "__main__":
+    main()
