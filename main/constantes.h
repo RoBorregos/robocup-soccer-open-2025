83bd94f11ea40e57becc88e25508580b1cc9f228
emilwinkp <181249743+emilwinkp@users.noreply.github.com>
2025-04-23 19:52:59 -0600

    Fixed constants for motors

--- main/constantes.h
+++ main/constantes.h
@@ -4,24 +4,24 @@
 
 
 
-const int MOTOR3_IN1 = 33; //34
-const int MOTOR3_IN2 = 34; //33
+const int MOTOR3_IN1 = 34; //34
+const int MOTOR3_IN2 = 33; //33
 const int MOTOR3_PWM = 4; 
 
-const int MOTOR4_IN1 = 31;  // 30
-const int MOTOR4_IN2 = 30;  // 31
+const int MOTOR4_IN1 = 30;  // 30
+const int MOTOR4_IN2 = 31;  // 31
 const int MOTOR4_PWM = 3;  // 
 
-const int MOTOR2_IN1 = 36;  // 
-const int MOTOR2_IN2 = 35;  // 
+const int MOTOR2_IN1 = 35;  // 36
+const int MOTOR2_IN2 = 36;  // 35
 const int MOTOR2_PWM = 5;  // 
 
-const int MOTOR1_IN1 = 29;  // 
-const int MOTOR1_IN2 = 28;  // 
+const int MOTOR1_IN1 = 28;  // 29
+const int MOTOR1_IN2 = 29;  // 28
 const int MOTOR1_PWM = 2;  //
 
 const int KICKER_PIN =  32;//Poner el ping del Kicker
-
+ 
 
 // Receive signals
 const uint8_t RECEIVE_BNO = 's';
