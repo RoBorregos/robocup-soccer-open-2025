994010400e1aad093bf8bfc6fdfa5e8b5f5376d6
emilwinkp <181249743+emilwinkp@users.noreply.github.com>
2025-04-25 20:27:49 -0600

    Added normalization for distances in robot back

--- Vision/OpenMv_H7.py
+++ Vision/OpenMv_H7.py
@@ -13,8 +13,8 @@ from pyb import UART
 uart = UART(3, 115200, timeout_char=0)
 uart.init(115200, bits=8, parity=None, stop=1)
 threshold = (15, 85, 14, 127, 0, 127) #(49, 84, 16, 39, 6, 100)#9, 93, 26, 92, -34, 78)(43, 100, 14, 39, -53, 76) (16, 100, 12, 127, -11, 127) (0, 100, -128, 127, -128, 127)
-threshold_2 = (0, 35, 33, -10, -122, -14) #Azul
-threshold_1 = (41, 99, -52, 127, 15, 127) #(68, 100, 16, -10, -20, 116) #(41, 99, -52, 127, 15, 127) (99, 25, 20, -83, 127, 40) Amarillo esta bien para 0,0,0
+threshold_own = (0, 35, 33, -10, -120, -6) #Azul
+threshold_goal = (41, 99, -52, 127, 15, 127) #(68, 100, 16, -10, -20, 116) #(41, 99, -52, 127, 15, 127) (99, 25, 20, -83, 127, 40) Amarillo esta bien para 0,0,0
 
 # The reference point for angle and distance are set in the folloxing coordinates
 
@@ -45,7 +45,7 @@ def initialize_open():
 
 
 def find_ball(img):
-    blob_ball = img.find_blobs([threshold], area_threshold=30, merge=True) #Checar los valores de area y pixeles
+    blob_ball = img.find_blobs([threshold], area_threshold=20, merge=True) #Checar los valores de area y pixeles
     for blob in blob_ball:
         img.draw_rectangle(blob.rect(), color = (255, 0, 0))
         img.draw_cross(blob.cx(), blob.cy(), color = (255, 0, 0))
@@ -53,7 +53,7 @@ def find_ball(img):
     return blob_ball
 
 def find_goal(img):
-    blob_goal = img.find_blobs([threshold_1], pixels_threshold=100, area_threshold=800, merge=True) #Checar los valores de area y pixeles
+    blob_goal = img.find_blobs([threshold_goal], pixels_threshold=100, area_threshold=200, merge=True) #Checar los valores de area y pixeles
     for blob in blob_goal:
         img.draw_rectangle(blob.rect(), color = (0, 255, 0))
         img.draw_cross(blob.cx(), blob.cy(), color = (0, 255, 0))
@@ -61,7 +61,7 @@ def find_goal(img):
     return blob_goal
 
 def find_goal_opp(img):
-    blob_goal_opp = img.find_blobs([threshold_2], pixels_threshold=100, area_threshold=800, merge=True) #Checar los valores de area y pixeles
+    blob_goal_opp = img.find_blobs([threshold_own], pixels_threshold=100, area_threshold=200, merge=True) #Checar los valores de area y pixeles
     for blob in blob_goal_opp:
         img.draw_rectangle(blob.rect(), color = (0, 0, 255))
         img.draw_cross(blob.cx(), blob.cy(), color = (0, 0, 255))
@@ -125,6 +125,10 @@ def main():
                 angle_ball = -(angle(blob) - 180)
                 #print("Distance Ball: %d" % distance_b)
                 print("Angle Ball: %d" % angle_ball)
+                if angle_ball < 5 and angle_ball > -5:
+                    angle_ball = 0
+                if angle_ball > 135 or angle_ball < -135:
+                    distance_b = distance_b - 30
 
         if blob_goal:
             for blob in blob_goal:
@@ -134,7 +138,9 @@ def main():
                 #print("Angle Goal: %d" % angle_goal)
                 if angle_goal < 5 and angle_goal > -5:
                     angle_goal = 0
-                    
+                if angle_goal > 135 or angle_goal < -135:
+                    distance_g = distance_g - 30
+
         elif not blob_goal:
             distance_g = 0
             angle_goal = 0
@@ -147,10 +153,12 @@ def main():
                 #print("Angle Opposite Goal: %d" % angle_goal)
                 if angle_gop < 5 and angle_gop > -5:
                     angle_gop = 0
-                    
+                if angle_gop > 135 or angle_gop < -135:
+                    distance_gop = distance_gop - 30
+
         elif not blob_goal_opp:
             distance_gop = 0
-            angle_gop = 0 
+            angle_gop = 0
         data = "{} {} {} {} {} {}\n".format(distance_b, angle_ball, distance_g, angle_goal, distance_gop, angle_gop)
         print("Sending: ", data)
         uart.write("{:.1f} {:.1f} {:.1f} {:.1f} {:.1f} {:.1f}\n".format(distance_b, angle_ball, distance_g, angle_goal, distance_gop, angle_gop))
