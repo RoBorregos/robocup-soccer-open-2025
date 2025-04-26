75eb5b4c50c57872ca01cf14ec1e5cf4ac0210f4
emilwinkp <181249743+emilwinkp@users.noreply.github.com>
2025-04-26 12:48:27 -0600

    Calibration of the photos

--- main/main.ino
+++ main/main.ino
@@ -3,38 +3,74 @@
 #include "constantes.h"
 #include "Bno.h"
 #include "PID.h"
+#include "Photo.h"
 #include <cmath>
+//Incluir la libreria del servo para el Kicker
 
 
 float bno_angle = 0;
 unsigned long start_millis;
 unsigned long current_millis;
 unsigned long previous_millis = 0;
+float cathethus = 0;
 float setpoint = 0 ;
 float translation_angle = 0;
 float adjust_angle = 0;
 float angle_degrees = 0;
 float ponderated_ball = 0;
 float ponderated_goal = 0;
+float ponderated_dribbler = 0;
 float ball_angle = 0;
+float dribbler_distance = 0;
+float dribbler_angle = 0;
 float goal_angle = 0;
 float ball_distance = 0;
 float last_distance = 0;
 float last_angle = 0;
 float distance = 0;
 float goal_distance = 0;
+float own_angle = 0;
+float own_distance = 0;
 float distance_pixels = 0;
 float differential_ball = 0;
 float differential_goal = 0;
+float differential_dribbler = 0;
 bool open_ball_seen = false;
-bool pixy_seen = false;
-const int BUFFER_SIZE = 50;
-char buffer[BUFFER_SIZE];
+bool dribbler_ball_seen = false;
+bool goal_seen = false;
+bool ball_captured = false;
+bool own_seen = false;
+const int BUFFER_SIZE = 100;
+char buffer1[BUFFER_SIZE];
+char buffer2[BUFFER_SIZE];
 const int servo_min = 1000;
-const int servo_mid = 1500;
+const int servo_mid = 1700;
 const int servo_max = 2000;
+int time_shoot = 2000;
+String serial1_line = "";
+String serial2_line = "";
+uint8_t front[2] = {A8, A9};
+uint8_t right[4] = {A3, A12, A13, A14};
+uint8_t left[4] = {A6, A15, A16, A17};
+uint8_t back[4] = {A0, A1, A2, A7};
 
+
+BNO055 bno;
 Servo dribbler;
+//Photo photo;
+
+
+PID pid(6, 0.01 , 0.6, 600); //0.6, 0.01, 0.6, 200
+PID pid2(3, 0.01 , 0.6, 500);
+
+
+Motors motors(
+    MOTOR1_PWM, MOTOR1_IN1, MOTOR1_IN2,
+    MOTOR2_PWM, MOTOR2_IN1, MOTOR2_IN2,
+    MOTOR3_PWM, MOTOR3_IN1, MOTOR3_IN2,
+    MOTOR4_PWM, MOTOR4_IN1, MOTOR4_IN2);
+    
+PhotoSensors sensors(front, left, right, back);
 
 void setup() {
     Serial1.begin(115200);
@@ -45,13 +81,8 @@ void setup() {
 
 
 void loop() {
-    dribbler.writeMicroseconds(servo_mid);
-    delay(1000);
-    dribbler.writeMicroseconds(servo_min);
-    delay(1000);
-    dribbler.writeMicroseconds(servo_max);
-    delay(1000);
-    dribbler.writeMicroseconds(0);
-    delay(5000);
+    float = sensors.getAverage(FRONT);
+    Serial.print("Valor de la foto: ");
+    Serial.println(valor);
 }
 
