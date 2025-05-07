cf943319eb399631b687525fd65a05f4383ab302
brinez-juan <A01782050@tec.mx>
2025-05-07 10:20:58 -0600

    cambio constantes motores

--- main/constantes.h
+++ main/constantes.h
@@ -4,16 +4,16 @@
 
 
 
-const int MOTOR3_IN1 = 34; //34
-const int MOTOR3_IN2 = 33; //33
+const int MOTOR3_IN1 = 33; //34
+const int MOTOR3_IN2 = 34; //33
 const int MOTOR3_PWM = 4; 
 
-const int MOTOR4_IN1 = 30;  // 30
-const int MOTOR4_IN2 = 31;  // 31
+const int MOTOR4_IN1 = 31;  // 30
+const int MOTOR4_IN2 = 30;  // 31
 const int MOTOR4_PWM = 3;  // 
 
-const int MOTOR2_IN1 = 35;  // 36
-const int MOTOR2_IN2 = 36;  // 35
+const int MOTOR2_IN1 = 36;  // 36
+const int MOTOR2_IN2 = 35;  // 35
 const int MOTOR2_PWM = 5;  // 
 
 const int MOTOR1_IN1 = 28;  // 29
